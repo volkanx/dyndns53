b686ab0ad30c31c852699b7c4f7a7b5a6eb519c8
Volkan <volkan@myvirtualhome.net>
2018-03-13 08:48:02 +0000

    Updated deployment script

--- src/DynDns53.Client.Angular5/deploy.sh
+++ src/DynDns53.Client.Angular5/deploy.sh
@@ -2,7 +2,7 @@
 
 ng build --prod --build-optimizer
 
-aws s3 rm s3://beta.dyndns53.myvirtualhome.net --recursive
-aws s3 cp ./dist/ s3://beta.dyndns53.myvirtualhome.net --recursive --profile dyndns53-s3
+aws s3 rm s3://dyndns53.myvirtualhome.net --recursive
+aws s3 cp ./dist/ s3://dyndns53.myvirtualhome.net --recursive --profile dyndns53-s3
 
 
