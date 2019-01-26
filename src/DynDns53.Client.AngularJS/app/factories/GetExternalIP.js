6283c895ac4230756ecf519230d8c01da3f6de32
Volkan <volkan@myvirtualhome.net>
2019-01-26 12:33:01 +0000

    Updated API URL

--- src/DynDns53.Client.AngularJS/app/factories/GetExternalIP.js
+++ src/DynDns53.Client.AngularJS/app/factories/GetExternalIP.js
@@ -1,5 +1,5 @@
 
 
 app.factory('GetExternalIP', function ($http) {
-  return $http.get('https://8x6c5h83d9.execute-api.eu-west-2.amazonaws.com/prod', { cache: false });
+  return $http.get('https://46ebv5trsk.execute-api.eu-west-2.amazonaws.com/prod', { cache: false });
 });
